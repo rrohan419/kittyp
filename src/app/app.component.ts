a7f5bd079e3449b7accbe011b19615f4d0f3077c
Rohan <rrohan419@gmail.com>
2023-07-01 20:59:50 +0530

    added picture and cards for benifits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,12 @@ import { NavigationEnd, Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   
+  contactsComponent : ContactsComponent = new ContactsComponent();
   hideFooter: boolean = false;
 
-  constructor(private router: Router){}
+  constructor(private router: Router){
+    console.log(this.contactsComponent.linkedInLink)
+  }
 
   ngOnInit() {
     this.router.events.subscribe(event => {
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
+  public linkedInLink : string  = "https://www.linkedin.com/company/kittyp/";
+  public instagramLink: string = "https://www.instagram.com/pookietalks_shit/";
+  public gmailLink: string = "mailto:kittypindia@gmail.com"
+
   constructor() { }
 
   ngOnInit(): void {
